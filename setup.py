93f5ad9ca48dcfa3f54ea9fa15817bf05a410e10
Paul Zwiessele <paul.zwiessele@fau.de>
2024-11-08 13:18:27 +0100

    Adding requirements.txt and setup.py, to complete project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework_2',
+    version='1.0',
+    packages=['random', 'unittest'],
+    url='',
+    license='',
+    author='Paul Zwiessele',
+    author_email='paul.zwiessele@fau.de',
+    description=''
+)
